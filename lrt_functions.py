caea150b0cc35f2f85853bec2ad725cd9df8bbf5
Amylavarapu <94078665+Amylavarapu@users.noreply.github.com>
2022-07-25 14:46:34 -0400

    Update and rename LRT_Method.py to lrt_functions.py

--- lrt_functions.py
+++ lrt_functions.py
@@ -10,46 +10,7 @@ import pandas as pd
 import numpy as np
 import logging
 
-''' This Code Pulls the product problem adverse events for a specific product code in a given time frame '''
-def procode_lrt_df(pro_code, lower_date: str = 'YYYY-MM-DD', upper_date: str = 'YYYY-MM-DD'):
-    df = pd.read_sql(f'''SELECT pma_pmn_number, date_received, date_of_event, date_report, mdr_report_key, adverse_event_flag, product_problems
-FROM "fda-open-database"."event", 
-    UNNEST(device) as t(devices)
-WHERE devices.device_report_product_code = '%s';'''%(pro_code),conn)
-    #df[['date_received','date_of_event','date_report']] = df[['date_received','date_of_event','date_report']].apply(pd.to_datetime,format = '%Y%m%d', errors='ignore')
-    df['date_received'] = pd.to_datetime(df['date_received'], errors='ignore',format = '%Y%m%d')
-    df['date_of_event'] = pd.to_datetime(df['date_of_event'], errors='ignore',format = '%Y%m%d')
-    df['date_report'] = pd.to_datetime(df['date_report'], errors='ignore',format = '%Y%m%d')
-    df = df.sort_values(by='date_of_event')
-#    df = df[['product_problems','pma_pmn_number']].loc[(df['date_of_event'] > lower_date) & (df['date_of_event'] < upper_date)].reset_index()
-    df = df[['product_problems','pma_pmn_number']].loc[(df['date_received'] > lower_date) & (df['date_received'] < upper_date)].reset_index()
-    df = df.rename(columns={'product_problems':'AEID'}).drop(['index'], axis = 1)
-    df['AEID'] = df['AEID'].str.replace(r'[][]', '', regex=True)
-    df['AEID'] = df['AEID'].str.split(',')
-    df = df.explode('AEID', ignore_index = True)
-    df = df.replace(r"^ +| +$", r"", regex=True)
-    return df
-    
-''' This Code Pulls the patient problem adverse events for a specific product code in a given time frame '''
-def patient_problem_df(pro_code, lower_date: str = 'YYYY-MM-DD', upper_date: str = 'YYYY-MM-DD'):
-    df = pd.read_sql(f'''SELECT pma_pmn_number, date_received, date_of_event, date_report, mdr_report_key, adverse_event_flag, patients.patient_problems
-FROM "fda-open-database"."event", 
-    UNNEST(device) as t(devices),
-    UNNEST(patient) as t(patients)
-WHERE devices.device_report_product_code = '%s';'''%(pro_code),conn)
-    #df[['date_received','date_of_event','date_report']] = df[['date_received','date_of_event','date_report']].apply(pd.to_datetime,format = '%Y%m%d', errors='ignore')
-    df['date_received'] = pd.to_datetime(df['date_received'], errors='ignore',format = '%Y%m%d')
-    df['date_of_event'] = pd.to_datetime(df['date_of_event'], errors='ignore',format = '%Y%m%d')
-    df['date_report'] = pd.to_datetime(df['date_report'], errors='ignore',format = '%Y%m%d')
-    df = df.sort_values(by='date_of_event')
-    df = df[['patient_problems','pma_pmn_number']].loc[(df['date_of_event'] > lower_date) & (df['date_of_event'] < upper_date)].reset_index()
-#    df = df[['patient_problems','pma_pmn_number']].loc[(df['date_received'] > lower_date) & (df['date_received'] < upper_date)].reset_index()
-    df = df.rename(columns={'patient_problems':'AEID'}).drop(['index'], axis = 1)
-    df['AEID'] = df['AEID'].str.replace(r'[][]', '', regex=True)
-    df['AEID'] = df['AEID'].str.split(',')
-    df = df.explode('AEID', ignore_index = True)
-    df = df.replace(r"^ +| +$", r"", regex=True)
-    return df
+
 
 def flogLRv(x, fnidot, fndotj, fndotdot):
     logLRvr = x*(np.log(x)-np.log(fnidot))
@@ -199,6 +160,8 @@ def calculate_lrt(
     logging.info(f'=== Finished LRT calculation. Resulting table contains {len(result)} rows.')
 
     return result
+
+
 
 
 def calculate_procode_lrt(pro_code, lower_date, upper_date):
