b99152b351195647d98df8b2531f445b83c060ed
Amylavarapu <94078665+Amylavarapu@users.noreply.github.com>
2022-07-25 15:37:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def calculate_predicate_lrt(k_number, lower_date, upper_date):
     k_number = k_number.upper()
     df = lrt_predicate_data.preds_lrt(k_number, lower_date, upper_date)
     df['problem type'] = 'Product Problem'
-    df2 = calculate_lrt(lrt_predicate_data.patient_problem_preds(k_number,lower_date ,upper_date),'AEID','pma_pmn_number','./S-11')
+    df2 = lrt_functions.calculate_lrt(lrt_predicate_data.patient_problem_preds(k_number,lower_date ,upper_date),'AEID','pma_pmn_number','./S-11')
     df2['problem type'] = 'Patient Problem'
     df = df.append(df2, ignore_index=True)
     df = df[['AEID','k_number','is_signal','pvalue','problem type']]        
@@ -23,9 +23,9 @@ def calculate_predicate_lrt(k_number, lower_date, upper_date):
   def calculate_procode_lrt(pro_code, lower_date, upper_date):
     pro_code = pro_code.upper()
     df = procode_data.procode_lrt_df(pro_code, lower_date, upper_date)
-    df = calculate_lrt(df, 'AEID', 'pma_pmn_number', './S-11')
+    df = lrt_functions.calculate_lrt(df, 'AEID', 'pma_pmn_number', './S-11')
     df['problem type'] = 'Product Problem'
-    df2 = calculate_lrt(procode_data.patient_problem_df(pro_code,lower_date ,upper_date),'AEID','pma_pmn_number','./S-11')
+    df2 = lrt_functions.calculate_lrt(procode_data.patient_problem_df(pro_code,lower_date ,upper_date),'AEID','pma_pmn_number','./S-11')
     df2['problem type'] = 'Patient Problem'
     df = df.append(df2, ignore_index=True)
     return df 
